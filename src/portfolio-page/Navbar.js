7b5c2c0ed87c4001e3fd75136a2e70854cee2685
upasana maurya <upasanamaurya01@gmail.com>
2023-02-03 05:17:00 +0530

    nevbar complete

--- src/portfolio-page/Navbar.js
+++ src/portfolio-page/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <>
       
-      <nav className="navbar navbar-expand-lg navbar-light bg-info">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container">
           <a className="navbar-brand" href="#">
             <img className="logo" src={logo} alt="logo..." />
           </a>
           
           <button
-            className="navbar-toggler"
+            className="navbar-toggler bg-light"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -20,7 +20,7 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon" />
+            <span className="navbar-toggler-icon " />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
